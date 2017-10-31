5a804b0e08fc42129f5e4b01921234c76a519f4e
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-10-31 21:25:53 +0200

    scaffold project

---
+++ DataflowICB/DataflowICB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataflowICB.Startup))]
+namespace DataflowICB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
