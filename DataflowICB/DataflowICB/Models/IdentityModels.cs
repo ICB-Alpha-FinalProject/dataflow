65a650b4dc6cccbff64cb6a89d2253a27c904214
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-11-02 18:50:43 +0200

    merge conflicts

--- DataflowICB/DataflowICB/Models/IdentityModels.cs
+++ DataflowICB/DataflowICB/Models/IdentityModels.cs
@@ -17,4 +17,17 @@ namespace DataflowICB.Models
             return userIdentity;
         }
     }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
 }
