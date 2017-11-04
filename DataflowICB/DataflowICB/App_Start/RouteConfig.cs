c9fb40b8d36b2634579dc044f51243d48b624fbc
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-11-04 18:07:19 +0200

    fix login and register page

--- DataflowICB/DataflowICB/App_Start/RouteConfig.cs
+++ DataflowICB/DataflowICB/App_Start/RouteConfig.cs
@@ -13,11 +13,15 @@ namespace DataflowICB
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.LowercaseUrls = true;
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
---
+++ DataflowICB/DataflowICB/Controllers/SensorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataflowICB.Controllers
+{
+    public class SensorController : Controller
+    {
+        public SensorController()
+        {
+        }
+
+        public ActionResult RegisterSensor()
+        {
+
+            return this.View();
+        }
+    }
+}
