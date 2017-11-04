3763ff284ead2247544b34a24abe231ba7339324
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-11-04 16:19:29 +0200

    add admin area

---
+++ DataflowICB/DataflowICB/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataflowICB.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        public AdminController()
+        {
+
+        }
+
+        public ActionResult AdminPanel()
+        {
+            return this.View();
+        }
+    }
+}
