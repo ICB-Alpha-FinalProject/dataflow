d57b64d5a81553da417afa61f13d30f5dc8fd5ca
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-11-07 17:25:59 +0200

    add dataservice project

---
+++ DataflowICB/Dataflow.DataServices/SensorService.cs
@@ -0,0 +1,13 @@
+﻿using Dataflow.DataServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dataflow.DataServices
+{
+    public class SensorService : ISensorService
+    {
+    }
+}
